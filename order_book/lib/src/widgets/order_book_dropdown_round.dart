f229b5133bf06c34327ef475991d577d8cb7d0ef
Igor Pashutin <igor.pashutin.georgia@gmail.com>
2024-03-07 18:50:01 +0400

    implement roundup dropdown

--- order_book/lib/src/widgets/order_book_dropdown_round.dart
+++ order_book/lib/src/widgets/order_book_dropdown_round.dart
@@ -2,10 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:order_book/order_book.dart';
 import 'package:order_book/src/bloc/order_book_bloc.dart';
 //import 'package:a4/app/widgets/inputs/drop_down_input.dart';
-
-
 
 class DropDownRound extends StatelessWidget {
   const DropDownRound({
@@ -18,22 +17,21 @@ class DropDownRound extends StatelessWidget {
       width: 50,
       child: BlocBuilder<OrderBookBloc, OrderBookState>(
         builder: (context, state) {
-          return Placeholder(fallbackHeight: 16,);
-          // return DropDownInput(
-          //   needSpacer: false,
-          //   showBorder: false,
-          //   fontSize: 12,
-          //   customPadding: const EdgeInsets.fromLTRB(6, 4, 6, 2),
-          //   customBorderRadius: BorderRadius.circular(6),
-          //   selectedIndex: OrderBookRound.values
-          //       .indexOf(state.control?.round ?? OrderBookRound.x001),
-          //   items: OrderBookRound.values.map((e) => e.getName()).toList(),
-          //   onChange: (index) {
-          //     final bloc = BlocProvider.of<OrderBookBloc>(context);
-          //     bloc.add(OrderBookEvent.roundChanged(
-          //         round: OrderBookRound.values[index]));
-          //   },
-          // );
+          return DropdownButton(
+            items: OrderBookRound.values
+                .map((e) => DropdownMenuItem(
+                      value: e,
+                      child: Text(e.getName()),
+                    ))
+                .toList(),
+            value: OrderBookRound.values[0],
+            onChanged: (OrderBookRound? value) {
+              if (value != null) {
+                final bloc = BlocProvider.of<OrderBookBloc>(context);
+                bloc.add(OrderBookEvent.roundChanged(round: value));
+              }
+            },
+          );
         },
       ),
     );
